283441f2adf251cc7244d7635ef8ab950255e24e
julicheruse <julicheruse@gmail.com>
2021-02-24 20:08:19 -0300

    LoginBox created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import LoginBox from "./components/LoginBox";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#80bad1",
+    },
+    secondary: {
+      main: "#a0aba3",
+    },
+  },
+
+  MuiTypography: {
+    variantMapping: {
+      h1: "h1",
+      h2: "h2",
+      h3: "h3",
+      h4: "h5",
+      h5: "h2",
+      h6: "h2",
+      subtitle1: "h2",
+      subtitle2: "h2",
+      body1: "span",
+      body2: "span",
+    },
+  },
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <LoginBox></LoginBox>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ client/src/components/LoginBox.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Typography, Button, TextField, Paper } from "@material-ui/core";
+
+export default function LoginBox() {
+  const handleSubmit = (e) => {
+    alert("submitiado");
+  };
+  return (
+    <Paper>
+      <form onSubmit={handleSubmit} style={{ padding: "100px" }}>
+        <Typography variant="h5">Welcome to My Chat APP</Typography>
+        <Typography variant="h6">What's your name?</Typography>
+        <TextField></TextField>
+        <br />
+        <Button type="submit" color="primary">
+          Enter Chat
+        </Button>
+      </form>
+    </Paper>
+  );
+}
